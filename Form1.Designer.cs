64bedcf7cc9a3e44a692d54f64f73d5315944496
SergioRibera <sergioalejandroriberacosta@gmail.com>
2020-10-10 09:28:05 -0400

    First Version

---
+++ Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Max_View_OBS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.imagePrev = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.camSelect = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePrev)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.imagePrev, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // imagePrev
+            // 
+            this.imagePrev.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imagePrev.Location = new System.Drawing.Point(3, 43);
+            this.imagePrev.Name = "imagePrev";
+            this.imagePrev.Size = new System.Drawing.Size(794, 404);
+            this.imagePrev.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagePrev.TabIndex = 0;
+            this.imagePrev.TabStop = false;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.01007F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.55919F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 5.541562F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 52.14106F));
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.camSelect, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.button1, 3, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(794, 34);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cámaras Disponibles: ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // camSelect
+            // 
+            this.camSelect.Dock = System.Windows.Forms.DockStyle.Top;
+            this.camSelect.FormattingEnabled = true;
+            this.camSelect.Location = new System.Drawing.Point(145, 7);
+            this.camSelect.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.camSelect.Name = "camSelect";
+            this.camSelect.Size = new System.Drawing.Size(188, 21);
+            this.camSelect.TabIndex = 1;
+            this.camSelect.SelectedIndexChanged += new System.EventHandler(this.camSelect_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ocultar de la barra de tareas";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Max View";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imagePrev)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox imagePrev;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox camSelect;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using AForge.Video;
+
+namespace Max_View_OBS
+{
+    public partial class Form1 : Form
+    {
+        bool hasDevice;
+        FilterInfoCollection devices;
+        VideoCaptureDevice captureDevice;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CloseCam();
+            LoadDevices();
+        }
+        void LoadDevices()
+        {
+            devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (devices.Count > 0)
+            {
+                hasDevice = true;
+                for (int i = 0; i < devices.Count; i++)
+                    camSelect.Items.Add(devices[i].Name.ToString());
+                camSelect.Text = devices[0].Name.ToString();
+            }
+            else
+                hasDevice = false;
+        }
+        void LoadCam()
+        {
+            if (hasDevice)
+            {
+                int i = camSelect.SelectedIndex;
+                string nameVideoCam = devices[i].MonikerString;
+                captureDevice = new VideoCaptureDevice(nameVideoCam);
+                captureDevice.NewFrame += new NewFrameEventHandler(CaptureVideo);
+                captureDevice.Start();
+            }
+            else
+                MessageBox.Show("No hay cámaras conectadas", "¡Alerta!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        public void CloseCam()
+        {
+            if (captureDevice != null && captureDevice.IsRunning)
+            {
+                captureDevice.SignalToStop();
+                captureDevice = null;
+            }
+        }
+        protected override void OnFormClosed(FormClosedEventArgs e)
+        {
+            base.OnFormClosed(e);
+            CloseCam();
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            CloseCam();
+        }
+
+        private void camSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CloseCam();
+            LoadCam();
+        }
+        void CaptureVideo(object sender, NewFrameEventArgs eventArgs)
+        {
+            if (hasDevice)
+            {
+                Bitmap img = (Bitmap)eventArgs.Frame.Clone();
+                imagePrev.Image = img;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e) => CloseCam();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.ShowInTaskbar = !this.ShowInTaskbar;
+            button1.Text = this.ShowInTaskbar ? "Ocultar de la barra de tareas" : "Mostrar en la barra de tareas";
+        }
+    }
+}
